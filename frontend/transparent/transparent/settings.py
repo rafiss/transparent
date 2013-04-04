b117d4de8b9ba8a1afea7fa03d1600fdcf415150
Rafi Shamim <rafiss@gmail.com>
2013-04-04 15:57:24 -0400

    Use dev DB on EC2 instance

--- frontend/transparent/transparent/settings.py
+++ frontend/transparent/transparent/settings.py
@@ -1,12 +1,21 @@
 # Django settings for transparent project.
+import os
 
-DEBUG = True
+if os.environ.get('DJANGO_PROD', None):
+    # prod environment specific
+    DEBUG = False
+    DB_HOST = ''
+    DB_NAME = 'transparent_db'
+else:
+    # development environment specific
+    DEBUG = True
+    DB_HOST = 'rafiss.com'
+    DB_NAME = 'transparent_dev_db'
+
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    ('Rafi Shamim', 'sshamim@princeton.edu'),
-    ('Abdulrahman Mahmoud', 'mahmoud@princeton.edu'),
-    ('Alexander Beebe', 'abeebe@princeton.edu'),
+    ('Rafi Shamim', 'admin@rafiss.com'),
     # ('Your Name', 'your_email@example.com'),
 )
 
@@ -15,24 +24,24 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'transparent_db',                      # Or path to database file if using sqlite3.
+        'NAME': DB_NAME,                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'transparent_user',
         'PASSWORD': 'trans333',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': DB_HOST,                 # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.transparent.rafiss.com']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Los_Angeles'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
