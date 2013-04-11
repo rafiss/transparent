81e60d39da3f6632fdb3d771a36d9d7dbb038aa0
Rafi Shamim <rafiss@gmail.com>
2013-04-10 20:58:21 -0400

    Moved static and templates directory into project directory

--- frontend/transparent.com/transparent/settings.py
+++ frontend/transparent.com/transparent/settings.py
@@ -75,7 +75,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/var/www/transparent.com/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static').replace('\\','/')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -136,6 +136,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
 )
 
 INSTALLED_APPS = (
--- frontend/transparent.com/transparent/urls.py
+++ frontend/transparent.com/transparent/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from transparent.views import hello
+from django.contrib.auth.views import login, logout
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -9,8 +10,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'transparent.views.home', name='home'),
     # url(r'^transparent/', include('transparent.foo.urls')),
-    ('^hello/$', hello),
-
+    (r'^hello/$', hello),
+    (r'^accounts/login/$',  login),
+    (r'^accounts/logout/$', logout),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
