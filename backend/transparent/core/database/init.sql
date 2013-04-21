5ba92a7141964df89c0974e508c6b73e6412a34e
Ajay Roopakalu <ajay.roopakalu@gmail.com>
2013-04-21 02:12:29 -0400

    Add optimized stored procedures.
    At this time, the Java driver code does not use these stored procedures
    yet. These procedures should eventually replace the clumsy Java code
    that currently does inserts. SQL can do a much better job optimizing
    these queries than we can manually and it will reduce the amount of data
    sent over the local socket as well.
    
    In addition, setting the following in /etc/mysql/my.cnf can help
    greatly reduce "query end" times at the cost of slight durability loss on a
    crash:
    
    innodb_flush_log_at_trx_commit=2
    
    This only affects transactions.

--- backend/transparent/core/database/init.sql
+++ backend/transparent/core/database/init.sql
@@ -1,47 +1,106 @@
 DROP DATABASE IF EXISTS transparent;
 
 CREATE DATABASE transparent;
-USE transparent;
 
 GRANT SELECT, INSERT, UPDATE, DELETE ON transparent.* TO 'darius'@'localhost';
 
-CREATE TABLE IF NOT EXISTS Metadata (
-    meta_key TEXT,
-    meta_value TEXT
+CREATE TABLE IF NOT EXISTS transparent.Metadata (
+    `meta_key` TEXT,
+    `meta_value` TEXT
 );
 
-CREATE TABLE IF NOT EXISTS Entity (
-    entity_id INT AUTO_INCREMENT,
-    name TEXT,
-    PRIMARY KEY (entity_id)
+CREATE TABLE IF NOT EXISTS transparent.Entity (
+    `entity_id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `name` TEXT
 );
 
-CREATE TABLE IF NOT EXISTS PropertyType (
-    property_type_id INT AUTO_INCREMENT,
-    property_name TEXT,
-    PRIMARY KEY (property_type_id)
+CREATE TABLE IF NOT EXISTS transparent.PropertyType (
+    `property_type_id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `property_name` TEXT
 );
 
-CREATE TABLE IF NOT EXISTS Property (
-    property_id INT AUTO_INCREMENT,
-    entity_id INT,
-    property_type_id INT,
-    PRIMARY KEY (property_id)
+CREATE TABLE IF NOT EXISTS transparent.Property (
+    `property_id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `entity_id` INT NOT NULL,
+    `property_type_id` INT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Trait (
-    property_id INT UNIQUE,
-    value TEXT
+CREATE TABLE IF NOT EXISTS transparent.Trait (
+    `property_id` INT PRIMARY KEY NOT NULL,
+    `value` TEXT
 );
 
-CREATE OR REPLACE VIEW vModel AS
+CREATE OR REPLACE VIEW transparent.vModel AS
 SELECT
       e.entity_id AS EntityID
     , e.name AS EntityName
     , x.property_name AS PropertyName
     , t.value AS TraitValue
 FROM Entity           AS e
-JOIN Property         AS p ON p.entity_id        = p.entity_id
+JOIN Property         AS p ON e.entity_id        = p.entity_id
 JOIN PropertyType     AS x ON x.property_type_id = p.property_type_id
 LEFT JOIN Trait       AS t ON t.property_id      = p.property_id
 ;
+
+DELIMITER //
+
+CREATE PROCEDURE transparent.GetEntityId(
+    IN moduleProductId TEXT,
+    OUT generatedKey LONG)
+    SQL SECURITY INVOKER
+BEGIN
+    INSERT INTO Entity VALUES(NULL, moduleProductId);
+    SET generatedKey = LAST_INSERT_ID();
+END//
+
+CREATE PROCEDURE transparent.InsertNewAttribute(
+    IN moduleId TEXT,
+    IN entityId LONG,
+    IN keyName TEXT,
+    IN valueName TEXT)
+    SQL SECURITY INVOKER
+BEGIN
+    DECLARE propertyTypeId, propertyId INT;
+
+    SELECT property_type_id FROM PropertyType
+        WHERE property_name=keyName
+        INTO propertyTypeId;
+
+    IF propertyTypeId IS NULL THEN
+        INSERT INTO PropertyType VALUES(NULL, keyName);
+        SET propertyTypeId = LAST_INSERT_ID();
+    END IF;
+
+    SELECT property_id FROM Property
+        WHERE entity_id=entityId AND property_type_id=propertyTypeId
+        INTO propertyId;
+
+    IF propertyId IS NULL THEN
+        INSERT INTO Property VALUES(NULL, entityId, propertyTypeId);
+        SET propertyId = LAST_INSERT_ID();
+    END IF;
+
+    REPLACE INTO Trait VALUES(propertyId, valueName);
+END//
+
+CREATE PROCEDURE transparent.AddProductId(
+    IN moduleId TEXT,
+    IN moduleProductId TEXT)
+    SQL SECURITY INVOKER
+BEGIN
+    DECLARE alreadyExists, generatedEntityId INT;
+    DECLARE fieldName TEXT DEFAULT 'module_id';
+
+    SELECT EXISTS(SELECT 1 FROM vModel WHERE
+        PropertyName=fieldName AND
+        TraitValue=moduleId AND
+        EntityName=moduleProductId) INTO alreadyExists;
+
+    IF alreadyExists = 0 THEN
+        CALL transparent.GetEntityId(moduleProductId, generatedEntityId);
+        CALL transparent.InsertNewAttribute(
+            moduleId, generatedEntityId, fieldName, moduleId);
+    END IF;
+END//
+
+DELIMITER ;
